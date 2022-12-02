34738c69469cce75013c53b46a0c12d5404759dd
titof <hktitof@gmail.com>
2022-12-02 15:26:32 +0100

    remove autoplay

--- components/HeroSection/VideoSection.tsx
+++ components/HeroSection/VideoSection.tsx
@@ -7,7 +7,7 @@ export default function VideoSection() {
     <div className="col-md-6" style={{ marginRight: "-10px" }}>
       <div className="row">
         <div className="col-md-12">
-          <video style={{ width: "100%" }} controls autoPlay muted>
+          <video style={{ width: "100%" }} controls >
             <source src="img/ypred_promo.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
