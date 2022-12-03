58699898a4ebaefdf0def4da5f49168c82e0c7dc
titof <hktitof@gmail.com>
2022-12-03 15:30:03 +0100

    added different laptop wallet whitelist

--- config/whitelist/whitelist.ts
+++ config/whitelist/whitelist.ts
@@ -1,7 +1,11 @@
-
 /**
  * @Attention 1 : all addresses in this file should be in lowercase!!!!!!!!!
  * @Attention 2 : change Mumbai rpc URL to mainnet rpc URL before deployment, in HeroSection.tsx
- * @Attention 3 : change Mumbai chainId to mainnet chainId before deployment, BuySection.tsx 
+ * @Attention 3 : change Mumbai chainId to mainnet chainId before deployment, BuySection.tsx
  */
-export const whitelist=["0xcf2b38a21d528b3f498fbc7b1078a33deaa5e5ef","0x6fa6da462cba635b0193809332387cdc25df3e8d","0xa88f31c972b0ec5d43b415a1c3049119512b99e4"]
+export const whitelist = [
+  "0xcf2b38a21d528b3f498fbc7b1078a33deaa5e5ef",
+  "0x6fa6da462cba635b0193809332387cdc25df3e8d",
+  "0xa88f31c972b0ec5d43b415a1c3049119512b99e4",
+  "0x0a50bca232b2c5691e3af8a9aec2c3738f45844a"
+];
