3bec522e9168eeecbe652085ff944e588cc32b27
titof <hktitof@gmail.com>
2022-12-03 13:43:54 +0100

    added another wallet address for testing

--- config/whitelist/whitelist.ts
+++ config/whitelist/whitelist.ts
@@ -4,4 +4,4 @@
  * @Attention 2 : change Mumbai rpc URL to mainnet rpc URL before deployment, in HeroSection.tsx
  * @Attention 3 : change Mumbai chainId to mainnet chainId before deployment, BuySection.tsx 
  */
-export const whitelist=["0xcf2b38a21d528b3f498fbc7b1078a33deaa5e5ef","0x6fa6da462cba635b0193809332387cdc25df3e8d"]
+export const whitelist=["0xcf2b38a21d528b3f498fbc7b1078a33deaa5e5ef","0x6fa6da462cba635b0193809332387cdc25df3e8d","0xa88f31c972b0ec5d43b415a1c3049119512b99e4"]
